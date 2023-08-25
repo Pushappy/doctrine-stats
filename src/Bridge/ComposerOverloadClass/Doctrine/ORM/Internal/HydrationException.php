b26fc90d8bd6f7c651e556c87406656bbba1289a
marsoder <40952343+marsoder@users.noreply.github.com>
2023-08-25 15:22:42 +0200

    Add missing HydrationException

---
+++ src/Bridge/ComposerOverloadClass/Doctrine/ORM/Internal/HydrationException.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+namespace Doctrine\ORM\Internal\Hydration;
+
+class HydrationException extends \ComposerOverloadClass\Doctrine\ORM\Internal\Hydration\HydrationException
+{
+
+}
