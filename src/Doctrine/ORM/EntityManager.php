3932708348801a6b574ef8c7946a446dbcdf6598
marsoder <40952343+marsoder@users.noreply.github.com>
2024-08-08 08:45:38 +0200

    Dbal connection getEventManager deprecation

--- src/Doctrine/ORM/EntityManager.php
+++ src/Doctrine/ORM/EntityManager.php
@@ -6,10 +6,9 @@ namespace Steevanb\DoctrineStats\Doctrine\ORM;
 
 use Doctrine\Common\EventManager;
 use Doctrine\DBAL\Connection;
-use Doctrine\ORM\{
-    Configuration,
+use Doctrine\ORM\{Configuration,
     EntityManager as DoctrineEntityManager,
-    ORMException
+    Exception\MissingMappingDriverImplementation
 };
 use Doctrine\Persistence\Mapping\Driver\MappingDriver;
 use Steevanb\DoctrineStats\Doctrine\ORM\Proxy\ProxyFactory;
@@ -19,26 +18,7 @@ class EntityManager extends DoctrineEntityManager
     public function __construct(Connection $conn, Configuration $config, EventManager $eventManager)
     {
         if ($config->getMetadataDriverImpl() instanceof MappingDriver === false) {
-            throw ORMException::missingMappingDriverImpl();
-        }
-
-        switch (true) {
-            case (is_array($conn)):
-                $conn = \Doctrine\DBAL\DriverManager::getConnection(
-                    $conn,
-                    $config,
-                    $eventManager ?? new EventManager()
-                );
-                break;
-
-            case ($conn instanceof Connection):
-                if ($eventManager !== null && $conn->getEventManager() !== $eventManager) {
-                    throw ORMException::mismatchedEventManager();
-                }
-                break;
-
-            default:
-                throw new \InvalidArgumentException('Invalid argument: ' . $conn);
+            throw new MissingMappingDriverImplementation();
         }
 
         parent::__construct($conn, $config, $eventManager);
